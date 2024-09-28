8028523362fc212535d81ac8858b30f0748a09d0
yoryet <y.danoun@pharmatender.cl>
2024-09-28 13:47:04 -0300

    add firestoreutrl con funcion check user

--- app/src/main/java/com/example/mysumativa/firebase/FirestoreUtils.kt
+++ app/src/main/java/com/example/mysumativa/firebase/FirestoreUtils.kt
@@ -1,2 +1,30 @@
 package com.example.mysumativa.firebase
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+suspend fun checkUserExists(email: String, password: String): Boolean {
+    val firestore = FirebaseFirestore.getInstance()
+    val usersCollection = firestore.collection("users")
+
+    return try {
+        Log.d("FirestoreCheck", "Consultando Firestore con email: $email y password: $password")
+
+        // Realizar la consulta a la colección de usuarios donde el email y la contraseña coincidan
+        val querySnapshot = usersCollection
+            .whereEqualTo("email", email)
+            .whereEqualTo("password", password)
+            .get()
+            .await()
+
+        Log.d("FirestoreCheck", "Documentos encontrados: ${querySnapshot.size()}")
+
+        // Si hay un documento en la respuesta, significa que el usuario existe
+        !querySnapshot.isEmpty
+    } catch (e: Exception) {
+        Log.e("FirestoreCheck", "Error en la consulta a Firestore: ${e.message}")
+        e.printStackTrace()
+        false
+    }
+}
